8479bd0aafd6bf9fab2831b8d2911d01b2acd337
rodgeraraujo <rogerio.araujo@mail.com>
2019-12-08 23:25:22 -0300

    :zap: plugins and layout design

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <main class="default">
+    <transition name="layout" mode="out-in">
+      <nuxt></nuxt>
+    </transition>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'defaultLayout'
+}
+</script>
+
+<style lang="stylus">
+</style>
